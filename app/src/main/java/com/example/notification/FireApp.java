5d7ef5b619ac07af77838ebb946c67236645d57c
vishnu-hub <vishnupnambiar1999@gmail.com>
2020-04-10 15:18:20 +0530

    notification

---
+++ app/src/main/java/com/example/notification/FireApp.java
@@ -0,0 +1,15 @@
+package com.example.notification;
+
+import android.app.Application;
+
+import com.firebase.client.Firebase;
+
+public class FireApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Firebase.setAndroidContext(this);
+    }
+}
